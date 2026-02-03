f2b4ff50eab6dbe20777d3082901e6f002b63a20
sbiba <wwe30621@gmail.com>
2026-02-03 13:12:17 +0800

    Configure base path for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/your-repository-name/',
+  base: '/new-projecct/',
   plugins: [vue()],
 })
